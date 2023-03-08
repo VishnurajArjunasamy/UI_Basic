4bf1b980b659d8c608310e97034723552db39569
VishnurajArjunasamy <125632293+VishnurajArjunasamy@users.noreply.github.com>
2023-03-08 20:18:55 +0530

    Update scripts.js

--- closures/scripts/scripts.js
+++ closures/scripts/scripts.js
@@ -1,82 +1,89 @@
-const usersObj=[
-    {   'Name':'Ram',
-        'AccNum':123456789752,
-        'cardNum':4444555566667777,
-        'PIN':4433,
-        'AccBal':200,
-    },
-    {   'Name':'Kumar',
-        'AccNum':563456899751,
-        'cardNum':4444555577773333,
-        'PIN':4553,
-        'AccBal':2000,
-    },
-    {   'Name':'Arun',
-        'AccNum':883336789760,
-        'cardNum':9999222211110000,
-        'PIN':1234,
-        'AccBal':1120,
-    },
-    {   'Name':'Nadia',
-        'AccNum':123886789751,
-        'cardNum':4334555566665577,
-        'PIN':4003,
-        'AccBal':100,
-    },
-    {   'Name':'Selli',
-        'AccNum':100456789752,
-        'cardNum':4444115500667777,
-        'PIN':2333,
-        'AccBal':2500,
-    },
-]
-
-class Client{
-    constructor({Name,AccNum,cardNum,PIN,AccBal}){
-        this.Name=Name
-        this.AccNum=AccNum
-        this.cardNum=cardNum
-        this.PIN=PIN
-        this.AccBal=AccBal
-    }
-}
-
-const ram =new Client(usersObj[0])
-const kumar =new Client(usersObj[1])
-const arun =new Client(usersObj[2])
-const nadia =new Client(usersObj[3])
-const selli =new Client(usersObj[4])
-
-
-function ATM(cardNum,PIN,amount,clientObj){
-    if((cardNum==clientObj.cardNum)&&(PIN==clientObj.PIN)){
-        function withdrawCash(){
-            if(amount<=clientObj.AccBal) {
-                clientObj.AccBal=clientObj.AccBal-amount
-                console.log(`${clientObj.Name} -  Rupess ${amount} successfully withdrawan`)
-                console.log(`${clientObj.Name} - Ruepess ${clientObj.AccBal} is the balance`)
-            }
-            else console.log(`${user1.Name} - Insufficient balance`)
+function bank(cardNum,PIN,amount,user,operation){
+    //client details
+    const usersObj=[
+        {   'Name':'Ram',
+            'AccNum':123456789752,
+            'cardNum':4444555566667777,
+            'PIN':4433,
+            'AccBal':200,
+        },
+        {   'Name':'Kumar',
+            'AccNum':563456899751,
+            'cardNum':4444555577773333,
+            'PIN':4553,
+            'AccBal':2000,
+        },
+        {   'Name':'Arun',
+            'AccNum':883336789760,
+            'cardNum':9999222211110000,
+            'PIN':1234,
+            'AccBal':1120,
+        },
+        {   'Name':'Nadia',
+            'AccNum':123886789751,
+            'cardNum':4334555566665577,
+            'PIN':4003,
+            'AccBal':100,
+        },
+        {   'Name':'Selli',
+            'AccNum':100456789752,
+            'cardNum':4444115500667777,
+            'PIN':2333,
+            'AccBal':2500,
+        },
+    ]
+    //client class
+    class Client{
+        constructor({Name,AccNum,cardNum,PIN,AccBal}){
+            this.Name=Name
+            this.AccNum=AccNum
+            this.cardNum=cardNum
+            this.PIN=PIN
+            this.AccBal=AccBal
         }
-        return withdrawCash()
     }
-    else console.log('wrong credentials!')
-}
 
-function CDM(cardNum,PIN,amount,clientObj){
-    if((cardNum==clientObj.cardNum)&&(PIN==clientObj.PIN)){
-        function depositCash(){
-            clientObj.AccBal=amount+clientObj.AccBal
-            console.log(`${clientObj.Name} - Rupees ${amount} deposited`)
+    //creating and adding user
+    let users = []
+    const ram =new Client(usersObj[0])
+    users.push(ram)
+    const kumar =new Client(usersObj[1])
+    users.push(kumar)
+    const arun =new Client(usersObj[2])
+    users.push(arun)
+    const nadia =new Client(usersObj[3])
+    users.push(nadia)
+    const selli =new Client(usersObj[4])
+    users.push(selli)
+
+    let clientObj= users.find((ele)=>ele.Name==user)
+
+    function ATM(){
+        if(amount<=clientObj.AccBal) {
+            clientObj.AccBal=clientObj.AccBal-amount
+            console.log(`${clientObj.Name} -  Rupess ${amount} successfully withdrawan`)
             console.log(`${clientObj.Name} - Ruepess ${clientObj.AccBal} is the balance`)
+    }
+    else console.log(`${clientObj.Name} - Insufficient balance`)
+    }
+
+    function CDM(){
+        clientObj.AccBal=amount+clientObj.AccBal
+        console.log(`${clientObj.Name} - Rupees ${amount} deposited`)
+        console.log(`${clientObj.Name} - Ruepess ${clientObj.AccBal} is the balance`)
+    }
+
+    if((cardNum==clientObj.cardNum)&&(PIN==clientObj.PIN)){
+        if(operation=='withdraw'){
+            return ATM();
         }
-        return depositCash()
+        
+        if(operation=='deposit'){
+            return CDM();
+        }    
     }
     else console.log('wrong credentials!')
 }
 
 
-let withdraw = ATM(9999222211110000,1234,100,arun)
-
-let deposit=CDM(4444555566667777,4433,100,ram)
-
+bank(9999222211110000,1234,20,'Arun','withdraw')
